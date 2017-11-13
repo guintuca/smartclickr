36720bacb688972ae32c33a7afbbf587042c4736
Christina Guintu <guintuca@dukes.jmu.edu>
2017-11-13 09:34:41 -0500

    added a poll question page with multiselect answers

---
+++ src/pages/questionms/questionms.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+	selector: 'page-questionms',
+	templateUrl: 'questionms.html'
+})
+
+export class QuestionMsPage {
+	
+	constructor(public navCtrl: NavController) {}
+
+}
